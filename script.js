2177794aae40c8e3bb67f0ba38395bced925bdd1
varun7380 <100070421+varun7380@users.noreply.github.com>
2022-07-16 00:43:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var check = false;
+    $('.clicks').on('click', function() {
+        if (check == false) {
+            $('#box').addClass('show');
+            check = true;
+        } else {
+            $('#box').removeClass('show');
+            check = false;
+        }
+
+    })
+
+    $('.submission').on('click', function() {
+        $('#box').removeClass('show');
+    });
+
+    $('.closex').on('click', function() {
+        $('#box').removeClass('show');
+    })
+});
